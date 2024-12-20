b4931f49d422324136d501ca22aa5715b661945e
Ekaterina <katya.05.2004.km@gmail.com>
2024-12-21 01:30:35 +0600

    get_test

--- api/restful_booker_api.py
+++ api/restful_booker_api.py
@@ -1,7 +1,7 @@
 import allure
 from api.api import Api
 from model.auth_model import RequestAuthModel, ResponseAuthModel
-# from model.create_booking_model import RequestCreateModel
+from model.get_booking_model import RequestCreateModel
 
 
 class RestfulBookerApi(Api):
@@ -24,13 +24,18 @@ class RestfulBookerApi(Api):
     #    _TOKEN=api.get_payload(['token'])
         return api
 
-    # @allure.step('Обращение к post')
-    # def restful_create(self, param_request_body: RequestCreateModel):
-    #     return self.post(url=self._URL,
-    #                      endpoint=self._ENDPOINT_BOOKING,
-    #                      json_body=param_request_body.to_dict())
+    @allure.step('Обращение к post')
+    def restful_create(self, param_request_body: RequestCreateModel):
+        return self.post(url=self._URL,
+                         endpoint=self._ENDPOINT_BOOKING,
+                         json_body=param_request_body.to_dict())
 
-    # @allure.step('Обращение к delete')
-    # def restful_delete(self, booking_id: int, token: str):
-    #     return self.delete(url=self._URL,
-    #                        endpoint=self._ENDPOINT_BOOKING + str(booking_id), headers={'token': f'token={token}'})
+    @allure.step('Обращение к delete')
+    def restful_delete(self, booking_id: int, token: str):
+        return self.delete(url=self._URL,
+                           endpoint=self._ENDPOINT_BOOKING + str(booking_id), headers={'token': f'token={token}'})
+
+    @allure.step('Обращение к get')
+    def restful_get(self, user_id: int):
+        return self.get(url=self._URL,
+                        endpoint=self._ENDPOINT_BOOKING + str(user_id))
